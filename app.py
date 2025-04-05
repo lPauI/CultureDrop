162a3df611540c56a68451e2c8178a83808eee67
lPauI <lascupg13@gmail.com>
2025-04-05 22:36:40 +0300

    Added login and register

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, flash, session
 
 from dotenv import load_dotenv
 
 from flask_wtf import CSRFProtect, FlaskForm
 from flask_sqlalchemy import SQLAlchemy
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
 from sqlalchemy.sql.expression import func
+
+from bcrypt import hashpw, gensalt, checkpw
 
 import os
 
@@ -26,6 +30,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
 
 class Clothes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -35,11 +45,94 @@ class Clothes(db.Model):
     description = db.Column(db.Text, nullable=False)
 
 
+class Songs(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    cover_path = db.Column(db.String(200), nullable=False)
+    song_path = db.Column(db.String(200), nullable=False)
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=80)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+
 @app.route('/')
 def home():
+    is_logged = 'user_id' in session
+    
     clothes = Clothes.query.order_by(func.random()).all()
     
     return render_template('index.html', clothes=clothes)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if 'user_id' in session:
+        flash('You are already logged. Log out for a new account.', 'info')
+        return redirect(url_for('home'))
+    
+    form = RegistrationForm()
+    
+    if form.validate_on_submit():
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        
+        hashed_password = hashpw(password.encode('utf-8'), gensalt()).decode('utf-8')
+        
+        new_user = Users(username=username, email=email, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if 'user_id' in session:
+        flash('You are already logged in.', 'info')
+        return redirect(url_for('home'))
+    
+    form = LoginForm()
+    
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        
+        # Find user by email
+        user = Users.query.filter_by(email=email).first()
+        
+        # Check if user exists and password is correct
+        if user and checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+            # Set up session
+            session['user_id'] = user.id
+            session['email'] = user.email
+            
+            flash('Login successful!', 'success')
+            return redirect(url_for('home'))
+        
+        else:
+            flash('Invalid email or password. Please try again.', 'error')
+    
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    session.pop('email', None)
+    flash('Te-ai deconectat cu succes!', 'success')
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
