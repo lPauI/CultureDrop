d8625527f2aa9d6ac4a5f6d07900b6286520247d
lPauI <lascupg13@gmail.com>
2025-04-05 19:58:09 +0300

    Added clothes table

--- app.py
+++ app.py
@@ -24,10 +24,20 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
+class Clothes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        # db.drop_all()
+        db.create_all()
+    
     app.run(host='0.0.0.0', port=5000, debug=True)
