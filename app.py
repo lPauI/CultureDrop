924ab268d6fb92e7f0c255f69156265ae18f2e43
lPauI <lascupg13@gmail.com>
2025-04-05 19:51:58 +0300

    Connected the DB and added env var

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 from flask import Flask, render_template
 
+from dotenv import load_dotenv
+
+from flask_sqlalchemy import SQLAlchemy
+
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Initialize Flask app and configure database connection
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
+
+mysql_user = os.getenv('MYSQL_USER')
+mysql_password = os.getenv('MYSQL_PASSWORD')
+mysql_host = os.getenv('MYSQL_HOST')
+mysql_database = os.getenv('MYSQL_DATABASE')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{mysql_user}:{mysql_password}@{mysql_host}/{mysql_database}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
